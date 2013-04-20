b8f4fce7b16defccca1a12741eca65228060eddb
Patrick Adair <padair@he.net>
2013-04-19 22:39:01 -0700

    Crappy list-delete implementation.
    This one didn't even finish executing before I committed it.

--- words.py
+++ words.py
@@ -6,14 +6,16 @@
 import operator
 import timeit
 
+# Setup.
+thesewords = open('./words', 'r')
+words = thesewords.read().split('\n')
+
+
 # Collection.
 def _brute_force_v1():
     """
         Very simple brute-force solution (slow) to this one.
     """
-    thesewords = open('./words', 'r')
-    words = thesewords.read().split('\n')
-
     counts = {}
     for each in words:
         if each not in counts:
@@ -26,7 +28,32 @@ def _brute_force_v1():
         counts.iteritems(), key=operator.itemgetter(1), reverse=True)[:5]:
         print "%s: %s" % (each[0], each[1])
 
+
+def _remove_list_v2():
+    """
+        Count ahead of time and remove words to shorten processing.
+    """
+    tempwords = words
+    counts = {}
+    while True:
+        # Compute out how many occurrences we see of each word.
+        if not tempwords:
+            break
+
+        current_word = tempwords.pop()
+        count = len([x for x in tempwords if x == current_word])
+        counts[current_word] = count
+
+        tempwords = [x for x in tempwords if x != current_word]
+
+    # Sort the output.
+    for each in sorted(
+        counts.iteritems(), key=operator.itemgetter(1), reverse=True)[:5]:
+        print "%s: %s" % (each[0], each[1])
+
 if __name__ == '__main__':
     print "Brute-force approach:"
     print timeit.timeit('_brute_force_v1()', setup='from __main__ import _brute_force_v1', number=1)
     print ""
+    print "List-delete approach:"
+    print timeit.timeit('_remove_list_v2()', setup='from __main__ import _remove_list_v2', number=1)
