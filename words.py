9353ec07f743ab6fd6c48c10fc73855636387412
Patrick Adair <padair@he.net>
2013-04-20 00:01:23 -0700

    Working reducer script that doesn't keep a giant table in mem.
    (Well, you know, aside from the sorted list of words it keeps.)

--- words.py
+++ words.py
@@ -5,6 +5,7 @@
 # Imports.
 import operator
 import timeit
+import resource
 
 # Setup.
 thesewords = open('./words', 'r')
@@ -51,9 +52,45 @@ def _remove_list_v2():
         counts.iteritems(), key=operator.itemgetter(1), reverse=True)[:5]:
         print "%s: %s" % (each[0], each[1])
 
+
+def _map_reduce_v3():
+    """
+        Try map/reduce to count more efficiently.
+        Slower, but doesn't keep a giant dictionary in memory.
+
+        Still requires the words from the load state in mem, though.
+    """
+    s = sorted(words)
+
+
+    def reduce_step(a, b):
+        """
+            Keep a running top-5 total for the wordlist.
+            The "top-5" is stored as a dict of dict[word] = count
+        """
+        # Ensure that after processing of each one is done you
+        # drop the dict down to 5 entries.
+        if b not in a:
+            if len(a) > 5:
+                a = dict(sorted(a.iteritems(),
+                    key=operator.itemgetter(1), reverse=True)[:5])
+
+            a[b] = 1
+        else:
+            a[b] += 1
+
+        return a
+
+    # The reduce engine will actually return the last item additionally,
+    # so we need to remove it.
+    result = reduce(reduce_step, s, {})
+    for each in sorted(result.iteritems(), key=operator.itemgetter(1),
+        reverse=True)[:5]:
+        print "%s: %s" % (each[0], each[1])
+
 if __name__ == '__main__':
-    print "Brute-force approach:"
-    print timeit.timeit('_brute_force_v1()', setup='from __main__ import _brute_force_v1', number=1)
+    print "Map-reduce approach:"
+    print timeit.timeit('_map_reduce_v3()', setup='from __main__ import _map_reduce_v3', number=1)
+    rss = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
+    print "%s kB" % (rss / 1024 / 1024)
     print ""
-    print "List-delete approach:"
-    print timeit.timeit('_remove_list_v2()', setup='from __main__ import _remove_list_v2', number=1)
